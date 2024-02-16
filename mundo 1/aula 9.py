94c8dd7eac54d2e8d998edacfea8f095b4000ccd
Stanley Bernardo <stanleystanleybd@gmail.com>
2024-02-15 22:29:29 -0400

    aula 9

---
+++ mundo 1/aula 9.py
@@ -0,0 +1,14 @@
+frase= '  Curso em Vídeo Python  '
+print (frase[:14])
+
+print ('''Meu conselho é que se case. Se você arrumar uma boa esposa, será feliz; se arrumar uma esposa ruim, se tornará um filósofo.''')
+
+print (frase.upper().count('O'))
+
+print (len(frase.strip()))
+
+frase= frase.replace('Python','é muito legal')
+print (frase)
+
+div= frase.split()
+print (div[0][2])
