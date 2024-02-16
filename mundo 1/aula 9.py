bc3228011a53856098b1c4ba232975d77a2695cb
Stanley Bernardo <stanleystanleybd@gmail.com>
2024-02-16 17:18:29 -0400

    ex009

--- mundo 1/aula 9.py
+++ mundo 1/aula 9.py
@@ -12,3 +12,21 @@ print (frase)
 
 div= frase.split()
 print (div[0][2])
+
+nome= str(input('qual seu nome?').strip())
+nomes= nome.split()
+print ('Seu nome escrito em maiúsculo é:{}'.format(nome.upper()))
+print ('Seu nome escrito em minúsculo é:{}'.format(nome.lower()))
+print ('Seu nome tem {} letras.'.format(len(nome) - nome.count(' ')))
+print ('Seu primeiro nome é {} e seu nome tem {} letras.'.format(nomes[0], len(nomes[0])))
+
+
+num= int(input('digite um número:'))
+u= num // 1 &10
+d= num // 10 & 10
+c= num // 100 & 10
+m= num // 1000 & 10
+print ('unidade:{}'.format(u))
+print ('dezena:{}'.format(d))
+print ('centena:{}'.format(c))
+print ('milhar:{}'.format(m))
