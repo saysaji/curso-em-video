21787c78918460087092b794b4f59dd178b160de
Stanley Bernardo <stanleystanleybd@gmail.com>
2024-02-20 14:41:06 -0400

    ex 09

--- mundo 1/aula 9.py
+++ mundo 1/aula 9.py
@@ -22,11 +22,31 @@ print ('Seu primeiro nome é {} e seu nome tem {} letras.'.format(nomes[0], len(
 
 
 num= int(input('digite um número:'))
-u= num // 1 &10
-d= num // 10 & 10
-c= num // 100 & 10
-m= num // 1000 & 10
+u= num // 1 % 10
+d= num // 10 % 10
+c= num // 100 % 10
+m= num // 1000 % 10
 print ('unidade:{}'.format(u))
 print ('dezena:{}'.format(d))
 print ('centena:{}'.format(c))
 print ('milhar:{}'.format(m))
+
+
+cidade= str(input('qual cidade voce nasceu?')).strip()
+print (cidade[:5].upper() == 'SANTO')
+
+
+nomee= str(input('qual seu nome?')).strip()
+print ('seu nome tem "silva"?', ('silva' in nomee.lower()))
+
+
+frasee= str(input('digite uma frase:')).lower().strip()
+print ('A letra "a" apareceu {} vezes na frase.'.format(frasee.count('a')))
+print ('A primeira letra "a" aparece na {} posição.'.format(frasee.find('a')+1))
+print ('A ultima letra "a" aparece na {} posição.'.format(frasee.rfind('a')+1))
+
+
+nomeee= str(input('Qual seu nome?')).strip()
+no= nomeee.split()
+print ('Seu primeiro nome é {}'.format(no[0]))
+print ('Seu ultimo nome é {}'.format(no[len(no)-1]))
